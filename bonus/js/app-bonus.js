3cc9c18713b26bc852a50e50a34be0495e43823f
gegebe <ggirbes@gmail.com>
2023-11-01 12:46:55 +0100

    comentada

--- bonus/js/app-bonus.js
+++ bonus/js/app-bonus.js
@@ -8,18 +8,26 @@ async function obtainURL(){
   let urlDogAPI = "https://api.thedogapi.com/v1/images/search";
   let urlCatAPI = "https://api.thecatapi.com/v1/images/search";
   
+  /*
+  Ternario para discernir url, si el value es "gato" urlCatAPI, si no lo es urlDogAPI
+  */
   let url = (userSelect.value === "gato")? urlCatAPI : urlDogAPI;
   console.log(url);
 
+  //Devuelve la url escogida
   return url;
 }
 
+/*
+Pasamos la url como parámetro a la función
+*/
 async function obtainRandomAnimal(url){
 
   //let url = await obtainURL();
   let obtainAnimal = await fetch(url);
   let processedData = await obtainAnimal.json();
 
+  //Para entrar en el primer y único ítem de la Array
   let newAnimal = processedData[0];
 
   imgNewAnimal.src = newAnimal.url;
@@ -28,7 +36,12 @@ async function obtainRandomAnimal(url){
 
 }
 
+/*
+Llamámos las funciones secuencialmente
+*/
 buttonNewAnimal.addEventListener("click", async ()=>{
+  //Obtener url
   let url = await obtainURL();
+  //Pasar url por el fetch()
   await obtainRandomAnimal(url);
 } );
